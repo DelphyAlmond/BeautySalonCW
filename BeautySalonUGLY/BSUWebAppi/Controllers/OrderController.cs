7416a19b7193a65675d8a5fea6935013a4e8546e
Delphy Almond <delphyalmond@gmail.com>
2026-03-22 00:44:50 +0400

    Reapply "6.1 : Worker-structure fixed + Controllers done"
    This reverts commit c30ecd4fedb15a0330bcb6f1b9bf0cf7f9b6f86c.

---
+++ BeautySalonUGLY/BSUWebAppi/Controllers/OrderController.cs
@@ -0,0 +1,66 @@
+﻿using BSUcontractmodels.AdapterContracts;
+using BSUcontractmodels.BindingModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BSUWebAppi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class OrderController(IOrderAdapter adapter) : ControllerBase
+    {
+        private readonly IOrderAdapter _adapter = adapter;
+
+        // CRUD
+        [HttpGet("{id}")]
+        public IActionResult GetOrder(string id)
+        {
+            return _adapter.GetElement(id).GetResponse(Request, Response);
+        }
+
+        [HttpPost]
+        public IActionResult CreateOrder([FromBody] OrderBM model)
+        {
+            return _adapter.RegisterOrder(model).GetResponse(Request, Response);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateOrder([FromBody] OrderBM model)
+        {
+            return _adapter.UpdateOrder(model).GetResponse(Request, Response);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteOrder(string id)
+        {
+            return _adapter.RemoveOrder(id).GetResponse(Request, Response);
+        }
+
+        // Фильтрация
+        [HttpGet("by-date")]
+        public IActionResult GetOrdersByDateGap([FromQuery] DateTime from, [FromQuery] DateTime to)
+        {
+            return _adapter.GetOrdersByDateGap(from, to).GetResponse(Request, Response);
+        }
+
+        [HttpGet("by-worker/{workerId}")]
+        public IActionResult GetOrdersByWorker(string workerId, [FromQuery] DateTime from, [FromQuery] DateTime to)
+        {
+            return _adapter.GetOrdersByWorker(workerId, from, to).GetResponse(Request, Response);
+        }
+
+        [HttpGet("by-customer/{customerId}")]
+        public IActionResult GetOrdersByCustomer(string customerId, [FromQuery] DateTime from, [FromQuery] DateTime to)
+        {
+            return _adapter.GetOrdersByCustomer(customerId, from, to).GetResponse(Request, Response);
+        }
+
+        [HttpGet("by-product/{productId}")]
+        public IActionResult GetOrdersByProduct(string productId, [FromQuery] DateTime from, [FromQuery] DateTime to)
+        {
+            return _adapter.GetOrdersByProduct(productId, from, to).GetResponse(Request, Response);
+        }
+    }
+}
---
+++ BeautySalonUGLY/BSUWebAppi/Controllers/ProductController.cs
@@ -0,0 +1,52 @@
+﻿using BSUcontractmodels.AdapterContracts;
+using BSUcontractmodels.BindingModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BSUWebAppi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ProductController(IProductAdapter adapter) : ControllerBase
+    {
+        private readonly IProductAdapter _adapter = adapter;
+
+        [HttpGet]
+        public IActionResult GetAllRecords()
+        {
+            return _adapter.GetList().GetResponse(Request, Response);
+        }
+
+        [HttpPost("{data}")]
+        public IActionResult GetRecord(string data)
+        {
+            return _adapter.GetElement(data).GetResponse(Request, Response);
+        }
+
+        [HttpGet("by-manufacturer/{manufacturerId}")]
+        public IActionResult GetByManufacturer(string manufacturerId)
+        {
+            return _adapter.GetByManufacturer(manufacturerId).GetResponse(Request, Response);
+        }
+
+        [HttpPost]
+        public IActionResult Register([FromBody] ProductBM model)
+        {
+            return _adapter.RegisterProduct(model).GetResponse(Request, Response);
+        }
+
+        [HttpPost]
+        public IActionResult ChangeInfo([FromBody] ProductBM model)
+        {
+            return _adapter.ChangeProductInfo(model).GetResponse(Request, Response);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            return _adapter.RemoveProduct(id).GetResponse(Request, Response);
+        }
+    }
+}
